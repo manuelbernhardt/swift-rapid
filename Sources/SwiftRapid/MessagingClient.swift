0a1de7f843d29404b10e7f6386bf04d0e2b209ff
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2020-06-23 15:47:19 +0200

    Protocols for messaging

---
+++ Sources/SwiftRapid/MessagingClient.swift
@@ -0,0 +1,28 @@
+import Foundation
+import NIO
+
+/// The client side of the pluggable messaging API
+protocol MessagingClient {
+
+    /// Sends a message to a remote node with redelivery semantics
+    /// - Parameters:
+    ///   - recipient: the remote node to send the message to
+    ///   - msg: the message to send
+    /// - Returns: a future that returns a RapidResponse if the call was successful
+    ///
+   @discardableResult
+   func sendMessage(recipient: Endpoint, msg: RapidRequest) -> EventLoopFuture<RapidResponse>
+
+    /// Sends a message to a remote node with best-effort semantics
+    /// - Parameters:
+    ///   - recipient: the remote node to send the message to
+    ///   - msg: the message to send
+    /// - Returns: a future that returns a RapidResponse if the call was successful
+    @discardableResult
+    func sendMessageBestEffort(recipient: Endpoint, msg: RapidRequest) -> EventLoopFuture<RapidResponse>
+
+    /// Signals the messaging client that it should free up resources in use
+    func shutdown()
+
+
+}
---
+++ Sources/SwiftRapid/MessagingServer.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+/// The server side of the pluggable messaging API
+protocol MessagingServer {
+
+    /// Starts the messaging server
+    func start()
+
+    /// Signals the server that it should free all resources in use
+    func shutdown()
+
+    /// The messaging server is initialized before the cluster has been joined, because the
+    /// node needs the ability to receive messages as part of the join protocol.
+    /// Once the node has received a successful join response, the messaging server can start
+    /// forwarding incoming messages to the membership service.
+    ///
+    /// TODO
+    // func onMembershipServiceInitialized(membershipService: MembershipService)
+
+}
